5782905c2e7e9b6d2e93269eaa412c9e6d631a72
Jerich Alombro <jerich.alombro@fsoft.com.vn>
2020-02-19 11:08:51 +0800

    Updates

---
+++ precache-manifest.2b7f4fd67c0cdc0457cdf9ee781b685d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aabcb794fce83248ceec1cb7e2b00ead",
+    "url": "/huckberry-frontend/index.html"
+  },
+  {
+    "revision": "1738df702a40b32cee93",
+    "url": "/huckberry-frontend/static/css/main.877abdf0.chunk.css"
+  },
+  {
+    "revision": "59a561331b99acfe20a1",
+    "url": "/huckberry-frontend/static/js/2.e4cd12b0.chunk.js"
+  },
+  {
+    "revision": "eb16858e21cecd78a7f3de272899fffc",
+    "url": "/huckberry-frontend/static/js/2.e4cd12b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1738df702a40b32cee93",
+    "url": "/huckberry-frontend/static/js/main.8f4fee1b.chunk.js"
+  },
+  {
+    "revision": "182101d31c050a8af80c",
+    "url": "/huckberry-frontend/static/js/runtime-main.357c8756.js"
+  },
+  {
+    "revision": "8996b79028423e6f3797ae179a699466",
+    "url": "/huckberry-frontend/static/media/huckberry-logo.8996b790.svg"
+  },
+  {
+    "revision": "a630439026588e8cea5e11fd0378ff69",
+    "url": "/huckberry-frontend/static/media/icon-bag.a6304390.svg"
+  },
+  {
+    "revision": "9e18e374a89158c6d3c96dd11503030f",
+    "url": "/huckberry-frontend/static/media/icon-search.9e18e374.svg"
+  }
+]);
